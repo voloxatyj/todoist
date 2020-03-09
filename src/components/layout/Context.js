91903a86fd5eb6600e823b0d039cebba07c41a36
MedynskyyOstap <47416022+voloxatyj@users.noreply.github.com>
2020-03-09 19:19:44 +0200

    create Tasks component to view tasks

--- src/components/layout/Context.js
+++ src/components/layout/Context.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Sidebar } from './Sidebar'
+import { Tasks } from '../Tasks'
 
 export const Context = () => {
 	return (
 		<div>
 			<Sidebar />	
+			<Tasks />
 		</div>
 	)
 }
